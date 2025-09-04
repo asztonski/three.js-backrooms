f0fd49ff29631c93be25506618e51ff7fbec9af1
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-09-03 21:17:35 -0500

    feat: update dependencies and refactor App component to use react-three-fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { Outlet } from 'react-router';
-import { Header } from './components/header/Header';
-import { Footer } from './components/footer/Footer';
-import { Main } from './components/main/Main';
+import { Canvas } from '@react-three/fiber';
 
 export default function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-gray-200">
-      <Header />
-      <Main className="container mx-auto py-6">
-        <Outlet />
-      </Main>
-      <Footer />
-    </div>
+    <>
+      <Canvas>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
