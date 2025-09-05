1fcf7540b7930f77dd76d0611aaab0c550a2307a
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-09-05 16:04:12 -0500

    refactor: simplify Cube component by removing grab functionality and enhancing hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { useRef, useState } from 'react';
+import { MeshWobbleMaterial, OrbitControls, useHelper } from '@react-three/drei';
 import * as THREE from 'three';
 
 export default function App() {
@@ -12,25 +13,13 @@ export default function App() {
 
   const Cube = ({ position = [0, 0, 0], color, secondColor, size = [1, 1, 1] }: CubeProps) => {
     const ref = useRef<THREE.Mesh>(null);
-    const [isGrabbed, setIsGrabbed] = useState(false);
-    const [isHovered, setIsHovered] = useState(isGrabbed ? true : false);
+    const [isHovered, setIsHovered] = useState(false);
 
     useFrame((state, delta) => {
       if (ref.current) {
-        const speed = isHovered ? 1 : 0.2;
-        const positionX = isGrabbed ? state.mouse.x * 5 : position[0];
-        const positionY = isGrabbed ? state.mouse.y * 5 : position[1];
-        const mousePosX = state.mouse.x;
-        const mousePosY = state.mouse.y;
-
+        const speed = isHovered ? 0 : 1;
         ref.current.rotation.x += delta * speed;
         ref.current.rotation.y += delta * speed;
-        ref.current.position.x = isGrabbed
-          ? mousePosX * 7
-          : THREE.MathUtils.lerp(ref.current.position.x, positionX, 0.1);
-        ref.current.position.y = isGrabbed
-          ? mousePosY * 7
-          : THREE.MathUtils.lerp(ref.current.position.y, positionY, 0.1);
       }
     });
 
@@ -38,30 +27,39 @@ export default function App() {
       <mesh
         onPointerEnter={(event) => [event.stopPropagation(), setIsHovered(true)]}
         onPointerLeave={() => setIsHovered(false)}
-        onPointerDown={(event) => [event.stopPropagation(), setIsGrabbed(true)]}
-        onPointerUp={() => setIsGrabbed(false)}
         position={position}
         ref={ref}
       >
         <boxGeometry args={size} />
-        <meshStandardMaterial color={isHovered ? secondColor : color} />
+        <MeshWobbleMaterial
+          color={isHovered ? secondColor : color}
+          speed={1}
+          factor={isHovered ? 1 : 0}
+        />
       </mesh>
     );
   };
 
-  return (
-    <>
-      <Canvas>
-        <directionalLight position={[0, 0, 2]} />
-        <ambientLight />
+  const Scene = () => {
+    const directionalLightRef = useRef<THREE.DirectionalLight>(
+      null!,
+    ) as React.RefObject<THREE.DirectionalLight>;
 
-        <group position={[0, -1, 0]}>
-          <Cube size={[1, 1, 1]} position={[1, 0, 0]} color="royalblue" secondColor="lightblue" />
-          <Cube size={[1, 1, 1]} position={[-1, 0, 0]} color="lime" secondColor="green" />
-          <Cube size={[1, 1, 1]} position={[-1, 2, 0]} color="hotpink" secondColor="pink" />
-          <Cube size={[1, 1, 1]} position={[1, 2, 0]} color="purple" secondColor="violet" />
-        </group>
-      </Canvas>
-    </>
+    useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'red');
+
+    return (
+      <>
+        <directionalLight position={[0, 0, 2]} ref={directionalLightRef} />
+        <ambientLight />
+        <Cube size={[1, 1, 1]} position={[0, 0, 0]} color="purple" secondColor="violet" />
+        <OrbitControls />
+      </>
+    );
+  };
+
+  return (
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
 }
