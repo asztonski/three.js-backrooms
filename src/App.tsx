b22ed69c1548b8067e292a3239e968bf3771ff34
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-09-05 15:17:13 -0500

    feat: add second color prop to Cube component and enhance interaction with mouse events

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import * as THREE from 'three';
 
 export default function App() {
   type CubeProps = {
     position?: [number, number, number];
     color: string;
+    secondColor?: string;
     size: [number, number, number];
   };
 
-  const Cube = ({ position = [0, 0, 0], color, size = [1, 1, 1] }: CubeProps) => {
+  const Cube = ({ position = [0, 0, 0], color, secondColor, size = [1, 1, 1] }: CubeProps) => {
     const ref = useRef<THREE.Mesh>(null);
+    const [isGrabbed, setIsGrabbed] = useState(false);
+    const [isHovered, setIsHovered] = useState(isGrabbed ? true : false);
 
     useFrame((state, delta) => {
       if (ref.current) {
-        ref.current.rotation.x += delta;
-        ref.current.rotation.y += delta * 2;
+        const speed = isHovered ? 1 : 0.2;
+        const positionX = isGrabbed ? state.mouse.x * 5 : position[0];
+        const positionY = isGrabbed ? state.mouse.y * 5 : position[1];
+        const mousePosX = state.mouse.x;
+        const mousePosY = state.mouse.y;
+
+        ref.current.rotation.x += delta * speed;
+        ref.current.rotation.y += delta * speed;
+        ref.current.position.x = isGrabbed
+          ? mousePosX * 7
+          : THREE.MathUtils.lerp(ref.current.position.x, positionX, 0.1);
+        ref.current.position.y = isGrabbed
+          ? mousePosY * 7
+          : THREE.MathUtils.lerp(ref.current.position.y, positionY, 0.1);
       }
     });
 
     return (
-      <mesh position={position} ref={ref}>
+      <mesh
+        onPointerEnter={(event) => [event.stopPropagation(), setIsHovered(true)]}
+        onPointerLeave={() => setIsHovered(false)}
+        onPointerDown={(event) => [event.stopPropagation(), setIsGrabbed(true)]}
+        onPointerUp={() => setIsGrabbed(false)}
+        position={position}
+        ref={ref}
+      >
         <boxGeometry args={size} />
-        <meshStandardMaterial color={color} />
+        <meshStandardMaterial color={isHovered ? secondColor : color} />
       </mesh>
     );
   };
@@ -33,14 +55,12 @@ export default function App() {
         <directionalLight position={[0, 0, 2]} />
         <ambientLight />
 
-        {/* <group position={[0, -1, 0]}>
-          <Cube size={[1, 1, 1]} position={[1, 0, 0]} color="royalblue" />
-          <Cube size={[1, 1, 1]} position={[-1, 0, 0]} color="lime" />
-          <Cube size={[1, 1, 1]} position={[-1, 2, 0]} color="hotpink" />
-          <Cube size={[1, 1, 1]} position={[1, 2, 0]} color="purple" />
-        </group> */}
-
-        <Cube position={[0, 0, 0]} size={[1, 1, 1]} color="orange" />
+        <group position={[0, -1, 0]}>
+          <Cube size={[1, 1, 1]} position={[1, 0, 0]} color="royalblue" secondColor="lightblue" />
+          <Cube size={[1, 1, 1]} position={[-1, 0, 0]} color="lime" secondColor="green" />
+          <Cube size={[1, 1, 1]} position={[-1, 2, 0]} color="hotpink" secondColor="pink" />
+          <Cube size={[1, 1, 1]} position={[1, 2, 0]} color="purple" secondColor="violet" />
+        </group>
       </Canvas>
     </>
   );
